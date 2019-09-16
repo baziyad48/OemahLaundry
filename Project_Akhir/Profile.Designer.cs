77c496c30a62aa44a3665a4cdc7e83cc9759f90b
Wildanokt <oktavianwww@gmail.com>
2019-09-16 11:48:35 +0700

    nambah view profile

--- Project_Akhir/Profile.Designer.cs
+++ Project_Akhir/Profile.Designer.cs
@@ -28,12 +28,277 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Profile));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.nama_user = new System.Windows.Forms.Label();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.email_user = new System.Windows.Forms.Label();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.telepon_user = new System.Windows.Forms.Label();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel5 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.alamat_user = new System.Windows.Forms.Label();
+            this.flowLayoutPanel6 = new System.Windows.Forms.FlowLayoutPanel();
+            this.editFoto = new System.Windows.Forms.Button();
+            this.editProfile = new System.Windows.Forms.Button();
+            this.flowLayoutPanel7 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel8 = new System.Windows.Forms.FlowLayoutPanel();
+            this.riwayat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel4.SuspendLayout();
+            this.flowLayoutPanel5.SuspendLayout();
+            this.flowLayoutPanel6.SuspendLayout();
+            this.flowLayoutPanel7.SuspendLayout();
+            this.flowLayoutPanel8.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nama : ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(8, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 154);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.nama_user);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 13);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(5);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(155, 27);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // nama_user
+            // 
+            this.nama_user.AutoSize = true;
+            this.nama_user.Location = new System.Drawing.Point(58, 5);
+            this.nama_user.Name = "nama_user";
+            this.nama_user.Size = new System.Drawing.Size(59, 13);
+            this.nama_user.TabIndex = 4;
+            this.nama_user.Text = "nama_user";
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.label2);
+            this.flowLayoutPanel2.Controls.Add(this.email_user);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(13, 46);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Padding = new System.Windows.Forms.Padding(5);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(155, 27);
+            this.flowLayoutPanel2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Email : ";
+            // 
+            // email_user
+            // 
+            this.email_user.AutoSize = true;
+            this.email_user.Location = new System.Drawing.Point(55, 5);
+            this.email_user.Name = "email_user";
+            this.email_user.Size = new System.Drawing.Size(57, 13);
+            this.email_user.TabIndex = 4;
+            this.email_user.Text = "email_user";
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Controls.Add(this.label5);
+            this.flowLayoutPanel3.Controls.Add(this.telepon_user);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(13, 79);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Padding = new System.Windows.Forms.Padding(5);
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(155, 27);
+            this.flowLayoutPanel3.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 5);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Telepon : ";
+            // 
+            // telepon_user
+            // 
+            this.telepon_user.AutoSize = true;
+            this.telepon_user.Location = new System.Drawing.Point(69, 5);
+            this.telepon_user.Name = "telepon_user";
+            this.telepon_user.Size = new System.Drawing.Size(68, 13);
+            this.telepon_user.TabIndex = 4;
+            this.telepon_user.Text = "telepon_user";
+            // 
+            // flowLayoutPanel4
+            // 
+            this.flowLayoutPanel4.Controls.Add(this.flowLayoutPanel1);
+            this.flowLayoutPanel4.Controls.Add(this.flowLayoutPanel2);
+            this.flowLayoutPanel4.Controls.Add(this.flowLayoutPanel3);
+            this.flowLayoutPanel4.Controls.Add(this.flowLayoutPanel5);
+            this.flowLayoutPanel4.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(179, 8);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Padding = new System.Windows.Forms.Padding(10);
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(184, 154);
+            this.flowLayoutPanel4.TabIndex = 7;
+            // 
+            // flowLayoutPanel5
+            // 
+            this.flowLayoutPanel5.Controls.Add(this.label7);
+            this.flowLayoutPanel5.Controls.Add(this.alamat_user);
+            this.flowLayoutPanel5.Location = new System.Drawing.Point(13, 112);
+            this.flowLayoutPanel5.Name = "flowLayoutPanel5";
+            this.flowLayoutPanel5.Padding = new System.Windows.Forms.Padding(5);
+            this.flowLayoutPanel5.Size = new System.Drawing.Size(155, 27);
+            this.flowLayoutPanel5.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(8, 5);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Alamat : ";
+            // 
+            // alamat_user
+            // 
+            this.alamat_user.AutoSize = true;
+            this.alamat_user.Location = new System.Drawing.Point(62, 5);
+            this.alamat_user.Name = "alamat_user";
+            this.alamat_user.Size = new System.Drawing.Size(64, 13);
+            this.alamat_user.TabIndex = 4;
+            this.alamat_user.Text = "alamat_user";
+            // 
+            // flowLayoutPanel6
+            // 
+            this.flowLayoutPanel6.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanel6.Controls.Add(this.flowLayoutPanel4);
+            this.flowLayoutPanel6.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel6.Name = "flowLayoutPanel6";
+            this.flowLayoutPanel6.Padding = new System.Windows.Forms.Padding(5);
+            this.flowLayoutPanel6.Size = new System.Drawing.Size(371, 168);
+            this.flowLayoutPanel6.TabIndex = 8;
+            // 
+            // editFoto
+            // 
+            this.editFoto.Location = new System.Drawing.Point(8, 8);
+            this.editFoto.Name = "editFoto";
+            this.editFoto.Size = new System.Drawing.Size(75, 23);
+            this.editFoto.TabIndex = 9;
+            this.editFoto.Text = "Ganti Foto";
+            this.editFoto.UseVisualStyleBackColor = true;
+            // 
+            // editProfile
+            // 
+            this.editProfile.Location = new System.Drawing.Point(89, 8);
+            this.editProfile.Name = "editProfile";
+            this.editProfile.Size = new System.Drawing.Size(134, 23);
+            this.editProfile.TabIndex = 10;
+            this.editProfile.Text = "Edit Data Diri";
+            this.editProfile.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel7
+            // 
+            this.flowLayoutPanel7.Controls.Add(this.editFoto);
+            this.flowLayoutPanel7.Controls.Add(this.editProfile);
+            this.flowLayoutPanel7.Controls.Add(this.riwayat);
+            this.flowLayoutPanel7.Location = new System.Drawing.Point(3, 177);
+            this.flowLayoutPanel7.Name = "flowLayoutPanel7";
+            this.flowLayoutPanel7.Padding = new System.Windows.Forms.Padding(5);
+            this.flowLayoutPanel7.Size = new System.Drawing.Size(371, 40);
+            this.flowLayoutPanel7.TabIndex = 11;
+            // 
+            // flowLayoutPanel8
+            // 
+            this.flowLayoutPanel8.Controls.Add(this.flowLayoutPanel6);
+            this.flowLayoutPanel8.Controls.Add(this.flowLayoutPanel7);
+            this.flowLayoutPanel8.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel8.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel8.Name = "flowLayoutPanel8";
+            this.flowLayoutPanel8.Size = new System.Drawing.Size(383, 225);
+            this.flowLayoutPanel8.TabIndex = 12;
+            // 
+            // riwayat
+            // 
+            this.riwayat.Location = new System.Drawing.Point(229, 8);
+            this.riwayat.Name = "riwayat";
+            this.riwayat.Size = new System.Drawing.Size(99, 23);
+            this.riwayat.TabIndex = 13;
+            this.riwayat.Text = "Lihat Riwayat";
+            this.riwayat.UseVisualStyleBackColor = true;
+            // 
+            // Profile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(407, 248);
+            this.Controls.Add(this.flowLayoutPanel8);
+            this.Name = "Profile";
             this.Text = "Profile";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            this.flowLayoutPanel4.ResumeLayout(false);
+            this.flowLayoutPanel5.ResumeLayout(false);
+            this.flowLayoutPanel5.PerformLayout();
+            this.flowLayoutPanel6.ResumeLayout(false);
+            this.flowLayoutPanel7.ResumeLayout(false);
+            this.flowLayoutPanel8.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label nama_user;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label email_user;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label telepon_user;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label alamat_user;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel6;
+        private System.Windows.Forms.Button editFoto;
+        private System.Windows.Forms.Button editProfile;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel7;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel8;
+        private System.Windows.Forms.Button riwayat;
     }
 }
