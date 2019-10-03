7116c0524d53d12cbc7398488a241d63e34ec375
Wildanokt <oktavianwww@gmail.com>
2019-10-03 11:03:50 +0700

    update petugas level

--- Project_Akhir/FormPetugas.cs
+++ Project_Akhir/FormPetugas.cs
@@ -8,12 +8,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using MySql.Data.MySqlClient;
 
 namespace Project_Akhir
 {
     public partial class FormPetugas : Form
     {
         static string username;
+
+        static string connString = "datasource=127.0.0.1;port=3306;username=root;password=;database=oemah_laundry;SslMode=none";
+        MySqlConnection conn = new MySqlConnection(connString);
+
         public FormPetugas(string param)
         {
             InitializeComponent();
@@ -65,7 +70,52 @@ namespace Project_Akhir
 
         private void petugasToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            new FormTambahPetugas().Show();
+            string tipe = getTipePetugas();
+            switch (tipe)
+            {
+                case "admin":
+                    new FormTambahPetugas().Show();
+                    break;
+
+                case "umum":
+                    MessageBox.Show("Anda tidak memiliki hak akses pada fitur ini");
+                    break;
+
+                default:
+                    MessageBox.Show("Anda tidak memiliki hak akses pada fitur ini");
+                    break;
+            }
+        }
+
+        private string getTipePetugas()
+        {
+            string query = "SELECT tipe FROM petugas WHERE username=@username";
+            string tipe = "umum";
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.CommandTimeout = 60;
+                cmd.Parameters.AddWithValue("@username", username);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        tipe = reader["tipe"].ToString();
+                    }
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return tipe;
         }
     }
 }
