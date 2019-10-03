37a119cd9a42bfdcb94ac28791993bfaefbb5404
baziyad48 <huhummamalybaziyad@gmail.com>
2019-10-03 08:47:42 +0700

    Adding hot reload to FormLihatPemesanan (Petugas).

--- Project_Akhir/formLihatPemesanan.cs
+++ Project_Akhir/formLihatPemesanan.cs
@@ -26,6 +26,8 @@ namespace Project_Akhir
             listView1.FullRowSelect = true;
             username = param;
             label2.Text = param;
+
+            refresh();
         }
 
         private void formLihatPesanan_Load(object sender, EventArgs e)
@@ -35,45 +37,7 @@ namespace Project_Akhir
 
         private void button1_Click(object sender, EventArgs e)
         {
-            listView1.Items.Clear();
-
-            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=oemah_laundry;SslMode=none";
-            string query = "SELECT pm.id_pemesanan, pt.nama, pl.nama, pm.tipe_cucian, pm.barang_cucian, pm.berat, pm.harga, pm.tanggal_masuk, pm.tanggal_keluar, pm.`status` FROM pemesanan pm inner join petugas pt on pt.id_petugas = pm.id_petugas INNER JOIN pelanggan pl on pl.id_pelanggan = pm.id_pelanggan";
-
-            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
-            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
-            commandDatabase.CommandTimeout = 60;
-            MySqlDataReader reader;
-
-            listView1.Items.Clear();
-
-            try
-            {
-                databaseConnection.Open();
-
-                reader = commandDatabase.ExecuteReader();
-
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        string[] row = { reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetString(5), reader.GetString(6), reader.GetString(7), reader.GetString(8), reader.GetString(9), };
-                        var listViewItem = new ListViewItem(row);
-                        listView1.Items.Add(listViewItem);
-                    } 
-                }
-                else
-                {
-                    Console.WriteLine("No rows found.");
-                }
-
-                databaseConnection.Clone();
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            refresh();
         }
 
         
@@ -107,6 +71,8 @@ namespace Project_Akhir
             {
                 MessageBox.Show(ex.Message);
             }
+
+            refresh();
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -139,6 +105,8 @@ namespace Project_Akhir
             {
                 MessageBox.Show(ex.Message);
             }
+
+            refresh();
         }
 
         private void button4_Click(object sender, EventArgs e)
@@ -188,6 +156,49 @@ namespace Project_Akhir
             dateTimePicker1.Value = Convert.ToDateTime(listView1.SelectedItems[0].SubItems[8].Text);
 
             
+        }
+
+        private void refresh()
+        {
+            listView1.Items.Clear();
+
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=oemah_laundry;SslMode=none";
+            string query = "SELECT pm.id_pemesanan, pt.nama, pl.nama, pm.tipe_cucian, pm.barang_cucian, pm.berat, pm.harga, pm.tanggal_masuk, pm.tanggal_keluar, pm.`status` FROM pemesanan pm inner join petugas pt on pt.id_petugas = pm.id_petugas INNER JOIN pelanggan pl on pl.id_pelanggan = pm.id_pelanggan";
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            listView1.Items.Clear();
+
+            try
+            {
+                databaseConnection.Open();
+
+                reader = commandDatabase.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        string[] row = { reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetString(5), reader.GetString(6), reader.GetString(7), reader.GetString(8), reader.GetString(9), };
+                        var listViewItem = new ListViewItem(row);
+                        listView1.Items.Add(listViewItem);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
+                }
+
+                databaseConnection.Clone();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
